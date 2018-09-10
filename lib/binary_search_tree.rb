ea4035428ec6b24d18340f99897bd29dbfc1af11
Jordan Yu <jordanyu44@gmail.com>
2018-09-09 21:40:27 -0700

    Set up BinarySearchTree class

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -27,22 +27,45 @@ class BinarySearchTree
   end
 
   def delete(value)
-    
+    target = find(value)
+    children = node_children(target)
+    # case children.length
+    # when 1
+    #   children[0].parent = target.parent
+
+    # when 2
+
+    target.parent.disown(target)
+    target.value, target.parent = nil, nil
+    target.left, target.right = nil, nil
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
-    return maximum tree_node.right if tree_node.right
-    return tree_node.value
+    return maximum(tree_node.right) if tree_node.right
+    return tree_node
+  end
+
+  def minimum(tree_node = @root)
+    return minimum(tree_node.left) if tree_node.left
+    return tree_node
   end
 
   def depth(tree_node = @root)
+    return 0 unless tree_node && tree_node.value 
+    left_depth = tree_node.left ? depth(tree_node.left) + 1 : 0
+    right_depth = tree_node.right ? depth(tree_node.right) + 1: 0
+    return [left_depth, right_depth].max
   end 
 
   def is_balanced?(tree_node = @root)
+    return true if tree_node == nil
+    return false if (depth(tree_node.left) - depth(tree_node.right)).abs > 1
+    return is_balanced?(tree_node.left) && is_balanced?(tree_node.right)
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    start = minimum(tree_node)
   end
 
 
@@ -52,11 +75,18 @@ class BinarySearchTree
     case val <=> node.value
     when -1, 0
       return insertion_helper(node.left, val) if node.left 
-      node.left = BSTNode.new(val)
+      node.left = BSTNode.new(val, node)
     when 1
       return insertion_helper(node.right, val) if node.right
-      node.right = BSTNode.new(val)
+      node.right = BSTNode.new(val, node)
     end
+  end
+
+  def node_children(node)
+    children = []
+    children << node.left if node.left 
+    children << node.right if node.right 
+    children
   end
 
 end
--- lib/bst_node.rb
+++ lib/bst_node.rb
@@ -2,10 +2,18 @@ class BSTNode
   attr_reader :value
   attr_accessor :left, :right
 
-  def initialize(value)
+  def initialize(value, parent = nil)
     @value = value
     @left = nil
     @right = nil
-    @parent = nil
+    @parent = parent
+  end
+
+  def disown(node) 
+    if left == node 
+      left = nil
+    elsif right == node
+      right = nil
+    end
   end
 end

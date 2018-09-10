b76553f8264df8490439520d7d1bdc33fc0c1fa3
Jordan Yu <jordanyu44@gmail.com>
2018-09-09 20:25:31 -0700

    Build BSTNode class

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,21 +1,39 @@
-# There are many ways to implement these methods, feel free to add arguments 
-# to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
 
 class BinarySearchTree
+  attr_accessor :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    if !root 
+      self.root = BSTNode.new(value)
+    else 
+      insertion_helper(root, value)
+    end
   end
 
   def find(value, tree_node = @root)
+    case value <=> tree_node.value 
+    when 0
+      return tree_node
+    when -1
+      return find(value, tree_node.left) if tree_node.left
+    when 1
+      return find(value, tree_node.right) if tree_node.right
+    end
   end
 
   def delete(value)
+    
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    return maximum tree_node.right if tree_node.right
+    return tree_node.value
   end
 
   def depth(tree_node = @root)
@@ -29,6 +47,16 @@ class BinarySearchTree
 
 
   private
-  # optional helper methods go here:
+
+  def insertion_helper(node, val)
+    case val <=> node.value
+    when -1, 0
+      return insertion_helper(node.left, val) if node.left 
+      node.left = BSTNode.new(val)
+    when 1
+      return insertion_helper(node.right, val) if node.right
+      node.right = BSTNode.new(val)
+    end
+  end
 
 end
